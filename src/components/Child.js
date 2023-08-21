13a854d831ef8297205596a61cfc0f93b6728352
manbodhvikram <manbodhvikram@gmail.com>
2023-08-21 12:04:49 +0530

    submitting code-Mon Aug 21 2023 12:04:46 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -14,8 +14,8 @@ const Child=({cartItems,addItem,removeItem})=>{
             <ul>
              {cartItems.map((item,index) => (
                <li key={item.id}>
-                
-              <span id="itemName">{item.name}</span> 
+       
+              <span id="itemName">{item.name} - </span> 
               {/* <span>{index+1} - </span> */}
               <span id="itemPrice">${item.price}</span>
             {/* <button onClick={() => removeItem(item.id)}>Remove</button> */}
